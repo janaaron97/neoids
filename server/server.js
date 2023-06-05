9871a85788040e7be65633fd4b538127d54353d8
Aaron Janke <aaronjanke@Aarons-Air.lan>
2023-06-05 17:39:15 -0500

    Using s3 for image hosting

--- server/server.js
+++ server/server.js
@@ -6,65 +6,105 @@ const path = require('path');
 const uuid = require('uuid');
 const cors = require('cors');
 
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region:process.env.AWS_REGION
+});
+
 // This line is necessary to be able to process POST request data
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client-react/build')));
 
-app.post('/api/save', (req, res) => {
-    const { base64String, translation } = req.body;
-    console.log(req.body);
+app.post('/api/save', async (req, res) => {
+  const { base64String, translation } = req.body;
 
-    // Generate a unique filename for the image
-    const filename = `${translation}.png`;
-    // Remove the "data:image/png;base64," part of the Base64 string
-    const base64Data = base64String.replace(/^data:image\/png;base64,/, '');
-    // Decode the Base64 string into an image
-    const imageBuffer = Buffer.from(base64Data, 'base64');
-    // Determine the path where the image will be saved
-    const imagePath = path.resolve(__dirname, `./images/${filename}`);
-    // Write the image to the file system
-    fs.writeFileSync(imagePath, imageBuffer);
-    // Load the glyphs.json file
-    const rawdata = fs.readFileSync(path.resolve(__dirname, './glyphs.json'));
-    let glyphs = JSON.parse(rawdata);
-    // Add the new glyph to the array
-    glyphs.push({ imageUrl: `/images/${filename}`, translation });
-    // Write the updated glyphs array back to the glyphs.json file
-    fs.writeFileSync(path.resolve(__dirname, './glyphs.json'), JSON.stringify(glyphs, null, 2));
-    // Send a success response
-    res.sendStatus(200);
-  });
+  // Generate a unique filename for the image
+  const filename = `${translation}.png`;
+
+  // Remove the "data:image/png;base64," part of the Base64 string
+  const base64Data = base64String.replace(/^data:image\/png;base64,/, '');
+
+  // Decode the Base64 string into an image
+  const imageBuffer = Buffer.from(base64Data, 'base64');
+
+  const uploadParams = {
+      Bucket: 'neiods',
+      Key: filename,
+      Body: imageBuffer,
+      ContentType: 'image/png',
+      ACL: 'public-read'
+  };
+
+  try {
+      // Upload the image to S3
+      const { Location: imageUrl } = await S3.upload(uploadParams).promise();
+
+      // Load the glyphs.json file
+      const rawdata = fs.readFileSync(path.resolve(__dirname, './glyphs.json'));
+      let glyphs = JSON.parse(rawdata);
+
+      // Add the new glyph to the array
+      glyphs.push({ imageUrl, translation });
+
+      // Write the updated glyphs array back to the glyphs.json file
+      fs.writeFileSync(path.resolve(__dirname, './glyphs.json'), JSON.stringify(glyphs, null, 2));
+
+      // Send a success response
+      res.sendStatus(200);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while saving the image.');
+  }
+});
 
   app.use('/images', express.static(path.join(__dirname, 'images')));
 
   app.get('/api/searchAll', (req, res) => {
-    // Load the glyphs.json file
-    const rawdata = fs.readFileSync(path.resolve(__dirname, './glyphs.json'));
-    const glyphs = JSON.parse(rawdata).sort((a, b) => a.translation.localeCompare(b.translation));
-
+    const params = {
+      Bucket: 'neiods'
+    };
   
-    res.json(glyphs);
+    S3.listObjects(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Error listing objects in S3 bucket' });
+      } else {
+        const glyphs = data.Contents.map(item => {
+          // Extract the filename without the extension
+          const translation = path.parse(item.Key).name;
+          return { imageUrl: "https://neiods.s3.us-east-2.amazonaws.com/" + item.Key, translation };
+        });
+  
+        // Sort the glyphs
+        glyphs.sort((a, b) => a.translation.localeCompare(b.translation));
+  
+        res.json(glyphs);
+      }
+    });
   });
 
-  app.get('/api/search', (req, res) => {
-    const { translation } = req.query;
-  
-    // Load the glyphs.json file
-    const rawdata = fs.readFileSync(path.resolve(__dirname, './glyphs.json'));
-    const glyphs = JSON.parse(rawdata);
-  
-    // Filter the glyphs to only include ones with the specified translation
-    console.log(translation);
-    var results = glyphs.filter(glyph => glyph.translation == translation.toString().toLowerCase());
-    console.log(results);
-    // If there are no matching glyphs, return an empty array
-    if (!results) {
+app.get('/api/search', (req, res) => {
+  const { translation } = req.query;
+
+  // Load the glyphs.json file
+  const rawdata = fs.readFileSync(path.resolve(__dirname, './glyphs.json'));
+  const glyphs = JSON.parse(rawdata);
+
+  // Filter the glyphs to only include ones with the specified translation
+  var results = glyphs.filter(glyph => glyph.translation == translation.toString().toLowerCase());
+
+  // If there are no matching glyphs, return an empty array
+  if (!results) {
       results = [];
-    }
-  
-    res.json(results);
-  });
+  }
+
+  res.json(results);
+});
 
   app.get('*', function (req, res) {
     const index = path.join(__dirname, '../client-react/build', 'index.html');
@@ -72,4 +112,5 @@ app.post('/api/save', (req, res) => {
   });
   
 
-app.listen(process.env.PORT || 5000)
+app.listen(3001)
+//process.env.PORT || 5000
